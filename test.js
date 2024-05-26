1d43890a4c9f9bdd7d54e524a83e36e52bf5b2cf
bhargav dangar <166829440+bkd259322@users.noreply.github.com>
2024-05-26 22:54:21 +0530

    array prectise

---
+++ test.js
@@ -0,0 +1,71 @@
+// 1) Creating an array: We start by creating an array (list in Node) with some initial values.
+const name = ["bhargav", "hiten", "punit", "raj", "hitesh"];
+
+// 2) Accessing elements: We access elements of the array using their indices.
+
+
+
+// const name1 = name.map(charecter => charecter.name)
+// console.log(name1)
+
+// 3) Modifying elements: We change the value of an element at a specific index.
+
+
+
+// name [0] = "raj"
+// console.log(name)
+
+// 4) Adding elements: We add a new element to the end of the array.
+
+
+
+// let newElement = "bharatbhai";
+// name.push(newElement);
+// console.log(name);
+
+
+// 5) Removing elements: We remove an element from the array.
+
+
+
+// name.pop();
+// console.log(name)
+
+
+
+// 6) Iterating over an array: We loop through all elements of the array and print them.
+
+// for(let i=0; i<name.length; i++){
+//     console.log(name)
+// }
+
+
+
+// 7) Length of the array: We get the number of elements in the array.
+
+// console.log(name.length)
+
+
+// 8) Slicing an array: We create a sub-array by slicing the original array.
+
+
+
+// const subArray = name.slice(1, 4);
+// console.log(subArray);
+
+
+// 9) Sorting an array: We sort the array in ascending order.
+
+
+
+// name.sort((a,b) => a.localeCompare(b))
+// console.log(name)
+
+// 10) Reversing an array: We reverse the order of elements in the array.
+
+
+
+// name.reverse()
+// console.log(name)
+
+
